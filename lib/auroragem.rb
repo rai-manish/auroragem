fb6e243d4dc4b409a3a3c2813442fa03b36f2d61
rai-manish <manish.rai@w3villa.com>
2018-08-08 17:52:08 +0530

    change class into module

--- lib/auroragem.rb
+++ lib/auroragem.rb
@@ -1,42 +1,45 @@
-require 'auroragem/module/user'
-require 'auroragem/module/projects'
-require 'auroragem/module/tenant'
-require 'auroragem/module/compute_hmac_signature'
-require 'auroragem/module/format_request_string'
-require 'auroragem/module/design'
-require 'auroragem/module/consumptionpro'
-require 'auroragem/module/components'
-require 'auroragem/module/hash_helper'
-require 'auroragem/module/post_apis'
-require 'auroragem/module/apis'
+require 'auroragem/auroragem/user'
+require 'auroragem/auroragem/projects'
+require 'auroragem/auroragem/tenant'
+require 'auroragem/auroragem/compute_hmac_signature'
+require 'auroragem/auroragem/format_request_string'
+require 'auroragem/auroragem/design'
+require 'auroragem/auroragem/consumptionpro'
+require 'auroragem/auroragem/components'
+require 'auroragem/auroragem/hash_helper'
+require 'auroragem/auroragem/post_apis'
+require 'auroragem/auroragem/apis'
 
-class Auroragem
+module Auroragem
+	class Test
+
 	
-	include HashHelper
-	include PostApis
-	include Components
-	include Consumptionpro
-	include Projects
-	include Design
-	include Tenant
-    include User
-    include Apis
-	include FormatRequestString
-	include ComputeHmacSignature
+		include HashHelper
+		include PostApis
+		include Components
+		include Consumptionpro
+		include Projects
+		include Design
+		include Tenant
+	  include User
+	  include Apis
+		include FormatRequestString
+		include ComputeHmacSignature
 
-    attr_accessor :aurora_api_secret, :aurora_api_key
-	
-	def initialize(aurora_api_key, aurora_api_secret)
-		@aurora_api_secret = aurora_api_secret
-		@aurora_api_key = aurora_api_key
-	end
+	    attr_accessor :aurora_api_secret, :aurora_api_keys
+		
+		def initialize(aurora_api_key, aurora_api_secret)
+			@aurora_api_secret = aurora_api_secret
+			@aurora_api_key = aurora_api_key
+		end
 
-	def self.api_secret
-		@aurora_api_secret
-	end
+		def self.api_secret
+			@aurora_api_secret
+		end
 
-	def self.api_key
-		@aurora_api_key
+		def self.api_key
+			@aurora_api_key
+		end
 	end
 
 end
